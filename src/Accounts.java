382f0093dbd752c258738f35b1725a2cf1fa0244
Akash D <akashdgowda6361@gmail.com>
2023-11-26 23:56:51 +0530

    modified main, Accounts and accountManager class

--- src/Accounts.java
+++ src/Accounts.java
@@ -2,79 +2,98 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Accounts {
-    Connection connection;
-    Scanner scanner;
-
-    public Accounts(Connection connection, Scanner scanner) {
+    private Connection connection;
+    private Scanner scanner;
+    public Accounts(Connection connection, Scanner scanner){
         this.connection = connection;
         this.scanner = scanner;
+
     }
 
+
+
+
     public long open_account(String email){
-        if(!account_exits(email)){
-            String openAccountQuery = "INSERT INTO accounts(account_number, full_name, email, balance, security_pin)" +
-                    " VALUES(?,?,?,?)";
+        if(!account_exist(email)) {
+            String open_account_query = "INSERT INTO Accounts(account_number, full_name, email, balance, security_pin) VALUES(?, ?, ?, ?, ?)";
             scanner.nextLine();
-            System.out.println("Enter Full Name: ");
-            String fullName = scanner.nextLine();
-            System.out.println("Enter initial Deposit: ");
+            System.out.print("Enter Full Name: ");
+            String full_name = scanner.nextLine();
+            System.out.print("Enter Initial Amount: ");
             double balance = scanner.nextDouble();
             scanner.nextLine();
-            System.out.println("Enter Security pin: ");
-            String pin = scanner.nextLine();
-            try{
-                long accountNumber = generateAccountNumber();
-                PreparedStatement preparedStatement = connection.prepareStatement(openAccountQuery);
-                preparedStatement.setLong(1, accountNumber);
-                preparedStatement.setString(2,fullName);
-                preparedStatement.setString(3,email);
-                preparedStatement.setDouble(4,balance);
-                preparedStatement.setString(5,pin);
+            System.out.print("Enter Security Pin: ");
+            String security_pin = scanner.nextLine();
+            try {
+                long account_number = generateAccountNumber();
+                PreparedStatement preparedStatement = connection.prepareStatement(open_account_query);
+                preparedStatement.setLong(1, account_number);
+                preparedStatement.setString(2, full_name);
+                preparedStatement.setString(3, email);
+                preparedStatement.setDouble(4, balance);
+                preparedStatement.setString(5, security_pin);
                 int rowsAffected = preparedStatement.executeUpdate();
                 if (rowsAffected > 0) {
-                    System.out.println("Account created Successfully");
-                    return accountNumber;
-                }else{
-                    throw new RuntimeException("Account Creation Failed");
+                    return account_number;
+                } else {
+                    throw new RuntimeException("Account Creation failed!!");
                 }
             } catch (SQLException e) {
-                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
         }
-        throw new RuntimeException("Account Already Exists");
+        throw new RuntimeException("Account Already Exist");
+
     }
-    public long generateAccountNumber(){
+
+    public long getAccount_number(String email) {
+        String query = "SELECT account_number from Accounts WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return resultSet.getLong("account_number");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Account Number Doesn't Exist!");
+    }
+
+
+
+    private long generateAccountNumber() {
         try {
             Statement statement = connection.createStatement();
-            String query = "SELECT account_number FROM accounts ORDER BY account_number DESC LIMIT 1";
-            ResultSet resultSet = statement.executeQuery(query);
-            if(resultSet.next()){
-                long recent_account_number = resultSet.getLong("account_number");
-                return recent_account_number+1;
-            }else{
+            ResultSet resultSet = statement.executeQuery("SELECT account_number from Accounts ORDER BY account_number DESC LIMIT 1");
+            if (resultSet.next()) {
+                long last_account_number = resultSet.getLong("account_number");
+                return last_account_number+1;
+            } else {
                 return 10000100;
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());;
+        }catch (SQLException e){
+            e.printStackTrace();
         }
         return 10000100;
     }
 
-    public boolean account_exits(String email){
-        String query = "SELECT account_number FROM accounts WHERE email = ?";
+    public boolean account_exist(String email){
+        String query = "SELECT account_number from Accounts WHERE email = ?";
         try{
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,email);
+            preparedStatement.setString(1, email);
             ResultSet resultSet = preparedStatement.executeQuery();
             if(resultSet.next()){
                 return true;
             }else{
                 return false;
             }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        }catch (SQLException e){
+            e.printStackTrace();
         }
         return false;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,89 @@ public class Main {
             Connection connection = DriverManager.getConnection(url, username, password);
             Scanner scanner =new Scanner(System.in);
             User user = new User(connection, scanner);
-            Accounts account = new Accounts(connection, scanner);
+            Accounts accounts = new Accounts(connection, scanner);
             AccountManager accountManager = new AccountManager(connection, scanner);
+            // Like creating session
+            String email;
+            long account_number;
+            while(true){
+                System.out.println("*** WELCOME TO BANKING SYSTEM ***");
+                System.out.println();
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println("3. Exit");
+                System.out.println("Enter your choice: ");
+                int choice1 = scanner.nextInt();
+                switch (choice1){
+                    case 1:
+                        user.register();
+                        break;
+                    case 2:
+                        email = user.login();
+                        if(email!=null){
+                            System.out.println();
+                            System.out.println("User Logged In!");
+                            if(!accounts.account_exist(email)){
+                                System.out.println();
+                                System.out.println("1. Open a new Bank Account");
+                                System.out.println("2. Exit");
+                                if(scanner.nextInt() == 1) {
+                                    account_number = accounts.open_account(email);
+                                    System.out.println("Account Created Successfully");
+                                    System.out.println("Your Account Number is: " + account_number);
+                                }else{
+                                    break;
+                                }
+
+                            }
+                            account_number = accounts.getAccount_number(email);
+                            int choice2 = 0;
+                            while (choice2 != 5) {
+                                System.out.println();
+                                System.out.println("1. Debit Money");
+                                System.out.println("2. Credit Money");
+                                System.out.println("3. Transfer Money");
+                                System.out.println("4. Check Balance");
+                                System.out.println("5. Log Out");
+                                System.out.println("Enter your choice: ");
+                                choice2 = scanner.nextInt();
+                                switch (choice2) {
+                                    case 1:
+                                        accountManager.debit_money(account_number);
+                                        break;
+                                    case 2:
+                                        accountManager.credit_money(account_number);
+                                        break;
+                                    case 3:
+                                        accountManager.transfer_money(account_number);
+                                        break;
+                                    case 4:
+                                        accountManager.getBalance(account_number);
+                                        break;
+                                    case 5: //breaks the switch ladder
+                                        System.out.println("Logging out");
+                                        break;
+                                    default:
+                                        System.out.println("Enter Valid Choice!");
+                                        break;
+                                }
+                            }
+
+                        }
+                        else{
+                            System.out.println("Incorrect Email or Password!");
+                        }
+                        break; //pull - by adding this line i am still able o go to the main menu rather than  completely exiting the application
+                    case 3:
+                        System.out.println("THANK YOU FOR USING BANKING SYSTEM!!!");
+                        System.out.println("Exiting System!");
+                        return;
+                    default:
+                        System.out.println("Enter Valid Choice");
+                        break;
+                }
+            }
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
