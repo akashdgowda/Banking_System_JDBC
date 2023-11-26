17405d377f6738f1d4ed67b185f0427712d71bc0
Akash D <akashdgowda6361@gmail.com>
2023-11-26 14:53:15 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static final String url = "jdbc:mysql://localhost:3306/banking_system";
+    public static final String username = "root";
+    public static final String password = "MySQLPassword";
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
